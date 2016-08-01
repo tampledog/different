0368720ae913596389ecd4d0325854617440cb5d
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-08-01 17:40:35 +0300

    wegv

--- src/js/plagins/README for trollCounter.txt
+++ src/js/plagins/README for trollCounter.txt
@@ -7,29 +7,13 @@ html
 
                 <div class="item" ></div>
 
-                or
-
-                <div class="item"  data-end-num="10464"></div>
-
-                or
-
-                <div class="item" data-start-num="0" ></div>
-
-
-
-
-
-
 
 initialization in js
-
 
     $(document).ready(function(){
         $('.item').trollCounter();
     });
-
     initialization with parameters:
-
         $(document).ready(function(){
                 $('.item').trollCounter({
                     startN:20,
@@ -37,9 +21,15 @@ initialization in js
                     });
             });
 
+
+
+parameters can be dependencing at data-attribute 
 default parameters:
-            duration=3000;  integer(miliseconds)  
-            fps=25;    integer 
-            startN=0;  integer
-            endN=1000;  integer
-            separator=" "; string  (separator for thousand (99 999 999))
+            duration=3000;  integer(miliseconds)                             data-options-duration
+            fps=25;    integer                                               (no data-attribute)
+            startN=0;  integer                                               data-options-startN
+            endN=1000;  integer                                              data-options-endN
+            separator=" "; string  (separator for thousand (99 999 999))     data-options-separator
+
+
+             
--- src/js/plagins/trollCounter.js
+++ src/js/plagins/trollCounter.js
@@ -17,23 +17,27 @@
 
                 var startNum = options.startN;
                 var endNum = options.endN;
-                if (typeof object.attr('data-start-num') !== typeof undefined && object.attr('data-start-num') !== false){ startNum = parseInt(object.attr('data-start-num'));}
-                if (typeof object.attr('data-end-num') !== typeof undefined && object.attr('data-end-num') !== false){endNum = parseInt(object.attr('data-end-num'));  }
+                var duration = options.duration;
+                var separator = options.separator;
+                if (typeof object.attr('data-options-startN') !== typeof undefined && object.attr('data-options-startN') !== false){ startNum = parseInt(object.attr('data-options-startN'));}
+                if (typeof object.attr('data-options-endN') !== typeof undefined && object.attr('data-options-endN') !== false){endNum = parseInt(object.attr('data-options-endN'));  }
+                if (typeof object.attr('data-options-duration') !== typeof undefined && object.attr('data-options-duration') !== false){duration = parseInt(object.attr('data-options-duration'));  }
+                if (typeof object.attr('data-options-separator') !== typeof undefined && object.attr('data-options-separator') !== false){separator = object.attr('data-options-separator');  }
 
 
-                var interval = (endNum - startNum)/options.duration;
+                var interval = (endNum - startNum)/duration;
 
                 var currentNum = interval * (1000/options.fps);
                 var showNum =currentNum;
                 var timer = setInterval(function () {
 
                     var str = ""+(showNum-showNum%1);
-                    str = str.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1'+options.separator+'');
+                    str = str.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1'+separator+'');
                     object.text(str);
                     showNum = showNum + currentNum;
                     if (showNum>=endNum){
                         str = ""+endNum;
-                        str = str.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1'+options.separator+'');
+                        str = str.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1'+separator+'');
                         object.text(str);
                         clearInterval(timer);
                     }
